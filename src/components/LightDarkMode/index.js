2fd94b0f426a409dd2e951bb9c857123fa32d5aa
chinnaragamsetti <110217256+chinnaragamsetti@users.noreply.github.com>
2023-12-30 21:12:22 +0530

    Update index.js

--- src/components/LightDarkMode/index.js
+++ src/components/LightDarkMode/index.js
@@ -4,19 +4,20 @@ import './index.css'
 
 class LightDarkMode extends Component {
   state = {isLightmode: true}
+
   onChangemode = () => {
     this.setState(prevState => ({isLightmode: !prevState.isLightmode}))
   }
 
   render() {
     const {isLightmode} = this.state
-    const mode = isLightmode ? 'lightmode' : 'darkmode'
+    const mode = isLightmode ? 'darkmode' : 'lightmode'
     const buttontext = isLightmode ? 'Light Mode' : 'Dark Mode'
     return (
       <div className="maincontainer">
         <div className={` subcontainer ${mode}`}>
           <h1 className="heading">Click To Change Mode</h1>
-          <button className="button" onClick="onChangemode">
+          <button type="button" className="button" onClick={this.onChangemode}>
             {buttontext}
           </button>
         </div>
